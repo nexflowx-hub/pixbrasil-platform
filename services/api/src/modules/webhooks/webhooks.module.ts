83685f2aa626bd9635be790f553610ea6c2b9bfc
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-20 01:41:24 -0300

    feat(webhooks): wire financial core

--- services/api/src/modules/webhooks/webhooks.module.ts
+++ services/api/src/modules/webhooks/webhooks.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ProvidersModule } from "../providers/providers.module";
+import { FinancialCoreModule } from "../financial-core/financial-core.module";
 import { MerchantWebhooksModule } from "../merchant-webhooks/merchant-webhooks.module";
 import { WebhooksController } from "./webhooks.controller";
 import { WebhooksService } from "./webhooks.service";
 
 @Module({
-  imports: [ProvidersModule, MerchantWebhooksModule],
+  imports: [ProvidersModule, MerchantWebhooksModule, FinancialCoreModule],
   controllers: [WebhooksController],
   providers: [WebhooksService],
 })
