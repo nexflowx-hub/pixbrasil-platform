4ceabe8b0095edc6fe6c1d6807a8a5954298a7f7
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-20 02:03:10 -0300

    feat(webhooks): enable callback reconciliation worker

--- services/api/src/modules/webhooks/webhooks.module.ts
+++ services/api/src/modules/webhooks/webhooks.module.ts
@@ -4,10 +4,11 @@ import { FinancialCoreModule } from "../financial-core/financial-core.module";
 import { MerchantWebhooksModule } from "../merchant-webhooks/merchant-webhooks.module";
 import { WebhooksController } from "./webhooks.controller";
 import { WebhooksService } from "./webhooks.service";
+import { ProviderWebhookReconciliationWorker } from "./provider-webhook-reconciliation.worker";
 
 @Module({
   imports: [ProvidersModule, MerchantWebhooksModule, FinancialCoreModule],
   controllers: [WebhooksController],
-  providers: [WebhooksService],
+  providers: [WebhooksService, ProviderWebhookReconciliationWorker],
 })
 export class WebhooksModule {}
