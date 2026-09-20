9db86adace9e87e15b6c7ab9d8edd0ff2c82b876
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-20 01:54:35 -0300

    feat(admin): wire financial payout core

--- services/api/src/modules/admin/admin.module.ts
+++ services/api/src/modules/admin/admin.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "../auth/auth.module";
 import { ProvidersModule } from "../providers/providers.module";
+import { FinancialCoreModule } from "../financial-core/financial-core.module";
 import { AdminController } from "./admin.controller";
 import { AdminService } from "./admin.service";
 
 @Module({
-  imports: [AuthModule, ProvidersModule],
+  imports: [AuthModule, ProvidersModule, FinancialCoreModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
