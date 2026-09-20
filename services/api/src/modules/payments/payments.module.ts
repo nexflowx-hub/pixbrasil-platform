94cd759f2ba3378d26b21e4eaba28be5526e6324
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-20 09:32:04 -0300

    feat(payments): wire provider adapters into payment runtime

--- services/api/src/modules/payments/payments.module.ts
+++ services/api/src/modules/payments/payments.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { MerchantAuthModule } from "../merchant-auth/merchant-auth.module";
+import { ProvidersModule } from "../providers/providers.module";
 import { RoutingModule } from "../routing/routing.module";
 import { PaymentsController } from "./payments.controller";
 import { PaymentsService } from "./payments.service";
 
 @Module({
-  imports: [MerchantAuthModule, RoutingModule],
+  imports: [MerchantAuthModule, ProvidersModule, RoutingModule],
   controllers: [PaymentsController],
   providers: [PaymentsService],
 })
