64574efb33aed13f406f09aa2849e7a8c4a71f54
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-20 10:34:56 -0300

    feat(client): wire Finance Core into client portal

--- services/api/src/modules/client/client.module.ts
+++ services/api/src/modules/client/client.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ClientAuthModule } from "../client-auth/client-auth.module";
+import { FinanceModule } from "../finance/finance.module";
 import { ClientController } from "./client.controller";
 import { ClientService } from "./client.service";
 
 @Module({
-  imports: [ClientAuthModule],
+  imports: [ClientAuthModule, FinanceModule],
   controllers: [ClientController],
   providers: [ClientService],
 })
