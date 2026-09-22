9036bcdf941a06de6a66b4ad69cf32b90b39bb35
NeXFlowX <nexflowx.tech@gmail.com>
2026-09-22 02:29:16 -0300

    feat(client): wire developer and terminal service dependencies

--- services/api/src/modules/client/client.module.ts
+++ services/api/src/modules/client/client.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ClientAuthModule } from "../client-auth/client-auth.module";
 import { FinanceModule } from "../finance/finance.module";
+import { MerchantWebhooksModule } from "../merchant-webhooks/merchant-webhooks.module";
+import { PaymentsModule } from "../payments/payments.module";
 import { ClientController } from "./client.controller";
 import { ClientService } from "./client.service";
 
 @Module({
-  imports: [ClientAuthModule, FinanceModule],
+  imports: [ClientAuthModule, FinanceModule, MerchantWebhooksModule, PaymentsModule],
   controllers: [ClientController],
   providers: [ClientService],
 })
